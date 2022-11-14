1af0e710de795125f2d9295f30e4151cc14e9545
Ido Berkovich <idoberko2@gmail.com>
2022-11-14 17:48:05 +0200

    refactor

--- main.go
+++ main.go
@@ -1,127 +1,12 @@
 package main
 
 import (
-	"bufio"
-	"encoding/json"
 	"fmt"
-	"net/http"
-	"net/url"
 	"time"
 
 	"github.com/canhlinh/hlsdl"
-	"github.com/go-rod/rod"
-	"github.com/go-rod/rod/lib/launcher"
+	log "github.com/sirupsen/logrus"
 )
-
-// func findVideoUrl(siteUrl string) chan string {
-// 	c := colly.NewCollector()
-// 	resp := make(chan string)
-
-// 	c.OnRequest(func(r *colly.Request) {
-// 		url := r.URL.String()
-// 		if strings.HasPrefix(url, "https://rgevod.akamaized.net/vodedge") {
-// 			fmt.Println("found: " + url)
-// 			resp <- url
-// 		} else {
-// 			// fmt.Println("not matching: " + url)
-// 			// rawBody, err := ioutil.ReadAll(r.Body)
-// 			// if err != nil {
-// 			// 	panic(err)
-// 			// }
-// 			// fmt.Println(rawBody)
-// 		}
-// 	})
-
-// 	c.OnResponse(func(r *colly.Response) {
-// 		fmt.Println("response")
-// 		fmt.Println(string(r.Body))
-// 	})
-
-// 	c.Visit(siteUrl)
-// 	return resp
-// }
-
-func findVideoUrl(siteUrl string) string {
-	resp := make(chan string, 1)
-
-	// Headless runs the browser on foreground, you can also use flag "-rod=show"
-	// Devtools opens the tab in each new tab opened automatically
-	// l := launcher.New().
-	// 	NoSandbox(true).
-	// 	Headless(false).
-	// 	Devtools(true)
-
-	// defer l.Cleanup() // remove launcher.FlagUserDataDir
-
-	// curl := l.MustLaunch()
-
-	browser := rod.New().
-		// ControlURL(curl).
-		// Trace(true).
-		// SlowMotion(2 * time.Second).
-		MustConnect()
-	launcher.Open(browser.ServeMonitor(""))
-	defer browser.MustClose()
-
-	router := browser.HijackRequests()
-	defer router.MustStop()
-
-	router.MustAdd("https://trc.taboola.com*playlist.m3u8*", func(ctx *rod.Hijack) {
-		// router.MustAdd("*", func(ctx *rod.Hijack) {
-		url := ctx.Request.URL().Query().Get("data")
-		_ = ctx.LoadResponse(http.DefaultClient, true)
-		fmt.Println(url)
-		resp <- url
-	})
-
-	go router.Run()
-	browser.MustPage(siteUrl)
-
-	urlObj := <-resp
-	var taboolaUrlContainer taboolaReq
-	if err := json.Unmarshal([]byte(urlObj), &taboolaUrlContainer); err != nil {
-		panic(err)
-	}
-
-	taboolaUrl, err := url.Parse(taboolaUrlContainer.Url)
-	if err != nil {
-		panic(err)
-	}
-
-	vidUrl := taboolaUrl.Query().Get("videoUrl")
-
-	httpResp, err := http.Get(vidUrl)
-	if err != nil {
-		panic(err)
-	}
-
-	// body, err := ioutil.ReadAll(httpResp.Body)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println(string(body))
-
-	scanner := bufio.NewScanner(httpResp.Body)
-	nextLine := false
-	var streamUrl string
-	for scanner.Scan() {
-		line := scanner.Text()
-		fmt.Println(line)
-		if nextLine {
-			streamUrl = line
-			nextLine = false
-		}
-		if line == "#EXT-X-STREAM-INF:BANDWIDTH=1800000" {
-			nextLine = true
-		}
-
-	}
-
-	fmt.Println("done")
-	// <-time.After(3 * time.Minute)
-
-	return streamUrl
-}
 
 func download(url string) {
 	fmt.Println(url)
@@ -143,8 +28,9 @@ func main() {
 	p := NewParser()
 	url, err := p.FindDownloadLink("https://playbyplay.sport5.co.il/?GameID=125423&FLNum=16")
 	if err != nil {
-		panic(err)
+		log.WithError(err).Fatal("failed to find download link")
 	}
+
 	fmt.Println("found video url: " + url)
 	download(url)
 }
