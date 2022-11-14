15bd7d00d93aa11f5f9dd0806001c77d1b42b5e9
Ido Berkovich <idoberko2@gmail.com>
2022-11-14 17:21:38 +0200

    refactor

--- main.go
+++ main.go
@@ -140,7 +140,11 @@ func main() {
 	// url := "https://rgevod.akamaized.net/vodedge/_definst_/mp4:rge/bynet/sport5/sport5/PRV5/sc0YaB81bw/App/NM_VTR_TAK_RAINA_131122_2_1800.mp4/chunklist_b1800000.m3u8"
 	// url := "https://rgevod.akamaized.net/vodedge/_definst_/mp4:rge/bynet/sport5/sport5/PRV5/sc0YaB81bw/App/NM_VTR_TAK_KASH_131122_2_1800.mp4/chunklist_b1800000.m3u8"
 	// url := findVideoUrl("https://playbyplay.sport5.co.il/?GameID=125421&FLNum=16")
-	url := findVideoUrl("https://playbyplay.sport5.co.il/?GameID=125423&FLNum=16")
+	p := NewParser()
+	url, err := p.FindDownloadLink("https://playbyplay.sport5.co.il/?GameID=125423&FLNum=16")
+	if err != nil {
+		panic(err)
+	}
 	fmt.Println("found video url: " + url)
 	download(url)
 }
---
+++ parser.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"github.com/go-rod/rod"
+)
+
+type Parser interface {
+	FindDownloadLink(url string) (string, error)
+}
+
+func NewParser() Parser {
+	return &parser{}
+}
+
+type parser struct{}
+
+func (p *parser) FindDownloadLink(siteUrl string) (string, error) {
+	urlObj := p.findTaboolaTraceUrl(siteUrl)
+	var taboolaUrlContainer taboolaReq
+	if err := json.Unmarshal([]byte(urlObj), &taboolaUrlContainer); err != nil {
+		panic(err)
+	}
+
+	taboolaUrl, err := url.Parse(taboolaUrlContainer.Url)
+	if err != nil {
+		panic(err)
+	}
+
+	vidUrl := taboolaUrl.Query().Get("videoUrl")
+
+	httpResp, err := http.Get(vidUrl)
+	if err != nil {
+		panic(err)
+	}
+
+	// body, err := ioutil.ReadAll(httpResp.Body)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(string(body))
+
+	scanner := bufio.NewScanner(httpResp.Body)
+	nextLine := false
+	var streamUrl string
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+		if nextLine {
+			streamUrl = line
+			nextLine = false
+		}
+		if line == "#EXT-X-STREAM-INF:BANDWIDTH=1800000" {
+			nextLine = true
+		}
+
+	}
+
+	fmt.Println("done")
+	// <-time.After(3 * time.Minute)
+
+	return streamUrl, nil
+}
+
+func (p *parser) findTaboolaTraceUrl(siteUrl string) string {
+	resp := make(chan string, 1)
+	browser := rod.New().MustConnect()
+	defer browser.MustClose()
+
+	router := browser.HijackRequests()
+	defer router.MustStop()
+
+	router.MustAdd("https://trc.taboola.com*playlist.m3u8*", func(ctx *rod.Hijack) {
+		url := ctx.Request.URL().Query().Get("data")
+		_ = ctx.LoadResponse(http.DefaultClient, true)
+		resp <- url
+	})
+
+	go router.Run()
+	browser.MustPage(siteUrl)
+
+	return <-resp
+}
