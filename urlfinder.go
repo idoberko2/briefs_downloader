cb5cfadfd2cc54884edf5b5b9ab284d6bf9a4a30
Ido Berkovich <idoberko2@gmail.com>
2022-11-14 21:31:13 +0200

    minor fix

--- urlfinder.go
+++ urlfinder.go
@@ -3,6 +3,6 @@ package main
 import "regexp"
 
 func IsStreamUrl(txt string) bool {
-	r := regexp.MustCompile("^https:\\/\\/(www|playbyplay)\\.sport5\\.co\\.il.*$")
+	r := regexp.MustCompile(`^https:\/\/(www|playbyplay)\.sport5\.co\.il.*$`)
 	return r.MatchString(txt)
 }
